9c0479fb52be66e201720f8061b36d4f27b14035
Mauricio Lara <mlara@santa-priscila.com>
2026-02-10 23:47:35 -0500

    Añadiendo múltiples carpetas

---
+++ Models/Vocabulario.swift
@@ -0,0 +1,18 @@
+//
+//  Vocabulary.swift
+//  MedCare
+//
+//  Created by Romi on 17/01/26.
+//
+
+import Foundation
+
+struct Vocabulario: Decodable, Identifiable {
+    let id: Int
+    let nivel: Int
+    let palabra: String
+    let traduccion: String
+    let definicion: String
+    let oracion: String
+}
+
---
+++ Views/HomeView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var isLoggedIn: Bool
+
+    var body: some View {
+        VStack(spacing: 10) {
+            // Logo
+            Image("logo_medcare")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 250)
+
+            // Título
+            /*
+            Text("MedCare")
+                .font(.largeTitle)
+                .bold()
+                .padding(.bottom, 20)
+*/
+            // Botones
+            NavigationLink("💊 Medicinas", destination: MedicinasView())
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+/*
+            Button("Forzar refresh") {
+                AuthService.shared.refreshToken { success in
+                    print("Refresh token result:", success)
+                }
+            }
+            .buttonStyle(.bordered)
+*/
+            Button("🚪 Cerrar sesión") {
+                UserDefaults.standard.removeObject(forKey: "token")
+                isLoggedIn = false
+            }
+            .foregroundColor(.red)
+            .buttonStyle(.bordered)
+        }
+        .padding()
+        .navigationTitle("MedCare")
+        .navigationBarTitleDisplayMode(.inline) // evita duplicación de título
+        .onAppear {
+            print("🔐 HomeView cargado. Token:", UserDefaults.standard.string(forKey: "token") ?? "nil")
+            programarNotificaciones()
+        }
+    }
+    private func programarNotificaciones() {
+        print("🔔 Programando notificaciones desde HomeView…")
+        ApiService.shared.request(endpoint: "/medicinas") { result in
+            switch result {
+            case .success(let data):
+                if let medicinas = try? JSONDecoder().decode([Medicina].self, from: data) {
+                    for med in medicinas {
+                        NotificationService.shared.programar(medicina: med)
+                    }
+                    print("🔔 Todas las notificaciones programadas desde Home")
+                } else {
+                    print("⚠️ No se pudo decodificar medicinas")
+                }
+            case .failure(let error):
+                print("❌ Error cargando medicinas para notificaciones:", error)
+            }
+        }
+    }
+}
+
+
